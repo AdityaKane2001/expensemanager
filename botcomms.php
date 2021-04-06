f99d1358c821132cdb5e164eec243c95571022df
shantanu1021 <shantanudeshpande00@gmail.com>
2021-04-06 16:11:47 +0530

    String received

--- botcomms.php
+++ botcomms.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
-require_once 'pdo.php';
+//require_once 'pdo.php';
 $HTTP_TOKEN = '1751648833:AAGwoBQhbU6TXvLcfGX-EP6Yvk6CYXyF9Vw';
 
-
+$url='https://api.telegram.org/bot1751648833:AAGwoBQhbU6TXvLcfGX-EP6Yvk6CYXyF9Vw/getUpdates';
+$site = file_get_contents($url);
+echo($site);
+echo("<br><br><br>");
+var_dump(json_decode($site,true));
 
  ?>
